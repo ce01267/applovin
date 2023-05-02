f4e233c07c1bface9424bd4474d7da24cab5b073
ce01267 <ce01267@p.cartahd.com>
2023-05-02 17:50:24 +0900

    native project

---
+++ native/android/unityLibrary/src/main/java/com/unity3d/player/UnityPlayerActivity.java
@@ -0,0 +1,273 @@
+package com.unity3d.player;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.PixelFormat;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.os.Process;
+
+import com.applovin.mediation.MaxAd;
+import com.applovin.mediation.MaxError;
+import com.applovin.mediation.MaxReward;
+import com.applovin.mediation.MaxRewardedAdListener;
+import com.applovin.mediation.ads.MaxRewardedAd;
+import com.applovin.sdk.AppLovinSdk;
+import com.applovin.sdk.AppLovinSdkConfiguration;
+import com.applovin.sdk.AppLovinSdkSettings;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class UnityPlayerActivity extends Activity implements IUnityPlayerLifecycleEvents, MaxRewardedAdListener
+{
+    protected UnityPlayer mUnityPlayer; // don't change the name of this variable; referenced from native code
+
+    // Override this in your custom UnityPlayerActivity to tweak the command line arguments passed to the Unity Android Player
+    // The command line arguments are passed as a string, separated by spaces
+    // UnityPlayerActivity calls this from 'onCreate'
+    // Supported: -force-gles20, -force-gles30, -force-gles31, -force-gles31aep, -force-gles32, -force-gles, -force-vulkan
+    // See https://docs.unity3d.com/Manual/CommandLineArguments.html
+    // @param cmdLine the current command line arguments, may be null
+    // @return the modified command line string or null
+    protected String updateUnityCommandLineArguments(String cmdLine)
+    {
+        return cmdLine;
+    }
+
+    // Setup activity layout
+    @Override protected void onCreate(Bundle savedInstanceState)
+    {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+
+        String cmdLine = updateUnityCommandLineArguments(getIntent().getStringExtra("unity"));
+        getIntent().putExtra("unity", cmdLine);
+
+        mUnityPlayer = new UnityPlayer(this, this);
+        setContentView(mUnityPlayer);
+        mUnityPlayer.requestFocus();
+
+        createRewardedAd();
+    }
+
+    // When Unity player unloaded move task to background
+    @Override public void onUnityPlayerUnloaded() {
+        moveTaskToBack(true);
+    }
+
+    // Callback before Unity player process is killed
+    @Override public void onUnityPlayerQuitted() {
+    }
+
+    @Override protected void onNewIntent(Intent intent)
+    {
+        // To support deep linking, we need to make sure that the client can get access to
+        // the last sent intent. The clients access this through a JNI api that allows them
+        // to get the intent set on launch. To update that after launch we have to manually
+        // replace the intent with the one caught here.
+        setIntent(intent);
+        mUnityPlayer.newIntent(intent);
+    }
+
+    // Quit Unity
+    @Override protected void onDestroy ()
+    {
+        mUnityPlayer.destroy();
+        super.onDestroy();
+    }
+
+    // If the activity is in multi window mode or resizing the activity is allowed we will use
+    // onStart/onStop (the visibility callbacks) to determine when to pause/resume.
+    // Otherwise it will be done in onPause/onResume as Unity has done historically to preserve
+    // existing behavior.
+    @Override protected void onStop()
+    {
+        super.onStop();
+
+        if (!MultiWindowSupport.getAllowResizableWindow(this))
+            return;
+
+        mUnityPlayer.pause();
+    }
+
+    @Override protected void onStart()
+    {
+        super.onStart();
+
+        if (!MultiWindowSupport.getAllowResizableWindow(this))
+            return;
+
+        mUnityPlayer.resume();
+    }
+
+    // Pause Unity
+    @Override protected void onPause()
+    {
+        super.onPause();
+
+        MultiWindowSupport.saveMultiWindowMode(this);
+
+        if (MultiWindowSupport.getAllowResizableWindow(this))
+            return;
+
+        mUnityPlayer.pause();
+    }
+
+    // Resume Unity
+    @Override protected void onResume()
+    {
+        super.onResume();
+
+        if (MultiWindowSupport.getAllowResizableWindow(this) && !MultiWindowSupport.isMultiWindowModeChangedToTrue(this))
+            return;
+
+        mUnityPlayer.resume();
+    }
+
+    // Low Memory Unity
+    @Override public void onLowMemory()
+    {
+        super.onLowMemory();
+        mUnityPlayer.lowMemory();
+    }
+
+    // Trim Memory Unity
+    @Override public void onTrimMemory(int level)
+    {
+        super.onTrimMemory(level);
+        if (level == TRIM_MEMORY_RUNNING_CRITICAL)
+        {
+            mUnityPlayer.lowMemory();
+        }
+    }
+
+    // This ensures the layout will be correct.
+    @Override public void onConfigurationChanged(Configuration newConfig)
+    {
+        super.onConfigurationChanged(newConfig);
+        mUnityPlayer.configurationChanged(newConfig);
+    }
+
+    // Notify Unity of the focus change.
+    @Override public void onWindowFocusChanged(boolean hasFocus)
+    {
+        super.onWindowFocusChanged(hasFocus);
+        mUnityPlayer.windowFocusChanged(hasFocus);
+    }
+
+    // For some reason the multiple keyevent type is not supported by the ndk.
+    // Force event injection by overriding dispatchKeyEvent().
+    @Override public boolean dispatchKeyEvent(KeyEvent event)
+    {
+        if (event.getAction() == KeyEvent.ACTION_MULTIPLE)
+            return mUnityPlayer.injectEvent(event);
+        return super.dispatchKeyEvent(event);
+    }
+
+    // Pass any events not handled by (unfocused) views straight to UnityPlayer
+    @Override public boolean onKeyUp(int keyCode, KeyEvent event)     { return mUnityPlayer.injectEvent(event); }
+    @Override public boolean onKeyDown(int keyCode, KeyEvent event)   { return mUnityPlayer.injectEvent(event); }
+    @Override public boolean onTouchEvent(MotionEvent event)          { return mUnityPlayer.injectEvent(event); }
+    /*API12*/ public boolean onGenericMotionEvent(MotionEvent event)  { return mUnityPlayer.injectEvent(event); }
+
+
+
+    private MaxRewardedAd rewardedAd;
+    private int           retryAttempt;
+
+    void createRewardedAd()
+    {
+        AppLovinSdk.getInstance(this).setMediationProvider("max");
+
+        AppLovinSdkSettings settings = new AppLovinSdkSettings( this );
+        List<String> adUnitIds = new ArrayList<>();
+        adUnitIds.add( "2030efa11a2e5353" );
+
+        settings.setInitializationAdUnitIds( adUnitIds );
+
+        AppLovinSdk sdk = AppLovinSdk.getInstance( settings, this );
+        sdk.setMediationProvider( "max" );
+        sdk.initializeSdk( new AppLovinSdk.SdkInitializationListener() {
+            @Override
+            public void onSdkInitialized(final AppLovinSdkConfiguration config) {
+                rewardedAd = MaxRewardedAd.getInstance( "2030efa11a2e5353", UnityPlayerActivity.this );
+                rewardedAd.setListener( UnityPlayerActivity.this );
+
+                rewardedAd.loadAd();
+                sdk.showMediationDebugger();
+            }
+        } );
+
+
+
+    }
+
+    // MAX Ad Listener
+    @Override
+    public void onAdLoaded(final MaxAd maxAd)
+    {
+        // Rewarded ad is ready to be shown. rewardedAd.isReady() will now return 'true'
+
+        // Reset retry attempt
+        retryAttempt = 0;
+    }
+
+    @Override
+    public void onAdLoadFailed(final String adUnitId, final MaxError error)
+    {
+        // Rewarded ad failed to load
+        // We recommend retrying with exponentially higher delays up to a maximum delay (in this case 64 seconds)
+
+        retryAttempt++;
+        long delayMillis = TimeUnit.SECONDS.toMillis( (long) Math.pow( 2, Math.min( 6, retryAttempt ) ) );
+
+        new Handler().postDelayed(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                rewardedAd.loadAd();
+            }
+        }, delayMillis );
+    }
+
+    @Override
+    public void onAdDisplayFailed(final MaxAd maxAd, final MaxError error)
+    {
+        // Rewarded ad failed to display. We recommend loading the next ad
+        rewardedAd.loadAd();
+    }
+
+    @Override
+    public void onAdDisplayed(final MaxAd maxAd) {}
+
+    @Override
+    public void onAdClicked(final MaxAd maxAd) {}
+
+    @Override
+    public void onAdHidden(final MaxAd maxAd)
+    {
+        // rewarded ad is hidden. Pre-load the next ad
+        rewardedAd.loadAd();
+    }
+
+    @Override
+    public void onRewardedVideoStarted(final MaxAd maxAd) {} // deprecated
+
+    @Override
+    public void onRewardedVideoCompleted(final MaxAd maxAd) {} // deprecated
+
+    @Override
+    public void onUserRewarded(final MaxAd maxAd, final MaxReward maxReward)
+    {
+        // Rewarded ad was displayed and user should receive the reward
+    }
+}
